fc0d5ae8e5eb6137c4b88d5f88e53c53a17eb48e
sorrowpsalm <thegodofinsanity@gmail.com>
2022-10-25 20:52:02 +0530

    updated assignments

--- Assessments/M4 Lead (Ajith Tony A) Assessments/Assessment 1/script.js
+++ Assessments/M4 Lead (Ajith Tony A) Assessments/Assessment 1/script.js
@@ -1,3 +1,10 @@
+// function displayMessage() {
+//     window.alert("Your user creation form has been succesfully submitted!");
+// }
+
 function displayMessage() {
-    window.alert("Your user creation form has been succesfully submitted!");
+    var inputname = document.getElementById("name").value;
+    var inputmobile = document.getElementById("mobile").value;
+    var inputemail = document.getElementById("email").value;
 }
+
