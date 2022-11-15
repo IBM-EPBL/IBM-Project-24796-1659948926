560c8561cd3ef7f689f5eff21c08d0c33a2aec69
sorrowpsalm <thegodofinsanity@gmail.com>
2022-11-15 20:15:54 +0530

    updated sprint

--- Project Development/Sprint 1/app.py
+++ Project Development/Sprint 1/app.py
@@ -2,3 +2,70 @@ from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if(request.method == "POST"):
+
+        # get the data from the form
+        password = request.form['password']
+        email = request.form['email']
+
+        # initialize the cursor
+        signup_cursor = mysql.connection.cursor()
+
+        # check whether user already exists
+        user_result = signup_cursor.execute(
+            "SELECT * FROM USERS WHERE user_email=%s", [email]
+        )
+
+        if(user_result > 0):
+            data = signup_cursor.fetchone()
+            data_password = data[3]
+            if(verify_password(password, data_password)):
+                signup_cursor.close()
+                session['id'] = data[0]
+                session['name'] = data[1]
+                session['email'] = data[2]
+                return redirect(url_for("home"))
+            else:
+                return render_template('login.html', error=1)
+        else:
+            return render_template('login.html', error=2)
+    return render_template('index.html', error=3)
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    if(request.method == "POST"):
+
+        # get the data from the form
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        # hash the password
+        pw_hash = create_bcrypt_hash(password)
+
+        # initialize the cursor
+        signup_cursor = mysql.connection.cursor()
+
+        # check whether user already exists
+        user_result = signup_cursor.execute(
+            "SELECT * FROM USERS WHERE user_email=%s", [email]
+        )
+        if(user_result > 0):
+            signup_cursor.close()
+            return render_template('signup.html', error=True)
+        else:
+            # execute the query
+            signup_cursor.execute(
+                'INSERT INTO USERS(user_name,user_email,user_password,user_type) VALUES(%s,%s,%s,%s)', (
+                    name, email, str(pw_hash), "2"
+                )
+            )
+
+            mysql.connection.commit()
+            signup_cursor.close()
+            return redirect(url_for('login'))
+
+    return render_template('signup.html', error=False)
