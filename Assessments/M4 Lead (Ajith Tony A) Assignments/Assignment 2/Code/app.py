834f13b5b9a7319dcbd8083165e600b7a1a59f91
sorrowpsalm <thegodofinsanity@gmail.com>
2022-11-15 13:44:00 +0530

    updated to standard template

--- Assessments/M4 Lead (Ajith Tony A) Assignments/Assignment 2/Code/app.py
+++ Assessments/M4 Lead (Ajith Tony A) Assignments/Assignment 2/Code/app.py
@@ -1,56 +1,67 @@
 from flask import Flask, render_template, url_for, request, flash
-import ibm_db  
-  
+import ibm_db
+
 app = Flask(__name__)
 app.secret_key = "917719C121"
 
+
 def connectDB():
-    conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=6667d8e9-9d4d-4ccb-ba32-21da3bb5aafc.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30376;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=gpz48320;PWD=I0QehUSLbAE3oquc;","","")
+    conn = ibm_db.connect(
+        "DATABASE=bludb;HOSTNAME=6667d8e9-9d4d-4ccb-ba32-21da3bb5aafc.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30376;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=gpz48320;PWD=I0QehUSLbAE3oquc;",
+        "",
+        "",
+    )
     return conn
+
 
 @app.route("/")
 def root():
     return render_template("home.html", title="Home")
 
-@app.route("/login", methods=('POST', 'GET'))
+
+@app.route("/signin", methods=("POST", "GET"))
 def signin():
     error = None
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
         userDB = connectDB()
-        sql = "SELECT username FROM users WHERE password = '{0}' AND email = '{1}'".format(password, email)
+        sql = "SELECT username FROM users WHERE password = '{0}' AND email = '{1}'".format(
+            password, email
+        )
         stmt = ibm_db.exec_immediate(userDB, sql)
         findUser = ibm_db.fetch_assoc(stmt)
-
         if findUser == False:
             error = "Incorrect Username/Password."
-        
-        while findUser != False:
-            success = "Hey " + findUser["USERNAME"]
-            findUser = ibm_db.fetch_assoc(stmt)
 
         if error is None:
-            return render_template('home.html', title="Home", success=success)
+            Succ = "Hey " + findUser["username"]
+            return render_template("home.html", title="Home", success="Login Successful")
         flash(error)
 
-    return render_template('login.html', title='Login', error=error)
+    return render_template("signin.html", title="Sign In", error=error)
 
-@app.route("/register", methods=('POST', 'GET'))
+
+@app.route("/signup", methods=("POST", "GET"))
 def signup():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        rollno = request.form['rollno']
-        
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        rollno = request.form["rollno"]
+
         userDB = connectDB()
-        sql = "INSERT INTO users (email, username, rollno, password) VALUES ('{0}', '{1}', '{2}', '{3}');".format(email, username, rollno, password)
+        sql = "INSERT INTO users (email, username, rollno, password) VALUES ('{0}', '{1}', '{2}', '{3}');".format(
+            email, username, rollno, password
+        )
         ibm_db.exec_immediate(userDB, sql)
-        return render_template('home.html', title="Home", success="Registration Successful")
+        return render_template(
+            "home.html", title="Home", success="Registration Successful"
+        )
 
-    return render_template("register.html", title="Register")
+    return render_template("signup.html", title="Sign Up")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=False)
