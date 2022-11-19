b291e8fdb24e129b91faf8ec84d30e292c353e82
sorrowpsalm <thegodofinsanity@gmail.com>
2022-11-19 23:57:38 +0530

    finalising

--- Project Development/Sprint 4/admin flask app/app.py
+++ Project Development/Sprint 4/admin flask app/app.py
@@ -219,7 +219,7 @@ def send_mail(email):
         html_content='<img src="https://cdn-icons-png.flaticon.com/512/4329/4329979.png" height="30px" width="30px"><br><strong> Alert!! You are currently entering a Containment Zone. Please turn back immediately </strong>')
     try:
         sg = SendGridAPIClient(
-            "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")
+            "")
         response = sg.send(message)
         print(response.status_code)
         print(response.body)
